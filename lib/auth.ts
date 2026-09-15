91081c5d5a6898ad571a928692052b8c7c5806e1
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-09-15 16:16:36 +0530

    Add authentication routes and user registration logic

---
+++ lib/auth.ts
@@ -0,0 +1,92 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+
+export const authOption: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: "Email", type: "text"},
+                password: {label: "Password", type: "password"}
+            },
+
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password) {
+                    throw new Error("Missing email or password")
+                }
+
+                try {
+                    await connectToDatabase()
+                    const user = await User.findOne({email: credentials.email})
+
+                    if(!user){
+                        throw new Error("No user found with this")
+                    }
+
+                    const isValid = await bcrypt.hash(credentials.password, user.password)
+
+                    if(!isValid){
+                        throw new Error("Invalid password")
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email
+                    }
+
+                } catch (error) {
+                    console.error("Auth error: ", error)
+                    throw error
+                }
+
+            }
+        })
+  ],
+
+  callbacks: {
+    async jwt({token, user}){
+        if(user){
+            token.id = user.id
+        }
+        return token
+    },
+    async session({session, token, user}){
+        if(session){
+            session.user.id = token.id as string
+        }
+        return session;
+    }
+  },
+
+  pages: {
+    signIn: "/login",
+    error: "/login"
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60
+  },
+
+  secret: process.env.NEXTAUTH_SECRET
+
+};
+
+
+
+
+
+
+//ye github wala ha ye bhi kr sakta pr pr abhi credentials wala kr rha ha
+// import GithubProvider from "next-auth/providers/github"
+// providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID!,
+//       clientSecret: process.env.GITHUB_SECRET!,
+//     }),
+//     // ...add more providers here
+//   ]
